098b4d384cf4e938d5011acd444e5d1de862854c
IlhomAliyev <ilhomaliyev.gs@gmail.com>
2024-04-05 14:28:13 +0500

    getServerSide

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST,
+      },
+      {
+        hostname: "cdn.dummyjson.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/pages/test/[id].tsx
+++ src/pages/test/[id].tsx
@@ -1,38 +1,70 @@
 import { OpenGraphHead } from "@/components/OpenGraphHead";
 import { IGetProductsResponse, IProduct } from "@/types/ITestData.type";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Test = ({ data }: { data: IProduct }) => {
   console.log("data inside page : ", data);
 
-  const image = data?.images[0];
-
   return (
     <>
-      <OpenGraphHead imageURL={image} />
-      <h1 className="text-3xl font-bold text-center mt-10">Test</h1>
+      <OpenGraphHead imageURL={data?.images[0]} />
+      <h1 className="mt-10 text-center text-3xl font-bold">Test Page</h1>
+      <div>
+        <p>{data?.id}</p>
+        <p>{data?.title}</p>
+        <p>{data?.description}</p>
+        <p>{data?.price}</p>
+        <p>{data?.discountPercentage}</p>
+        <p>{data?.rating}</p>
+        <p>{data?.stock}</p>
+        <p>{data?.brand}</p>
+        <p>{data?.category}</p>
+
+        <Image src={data?.thumbnail} alt={data?.title} />
+
+        <div className="mx-auto mt-10 grid w-full max-w-6xl grid-cols-3 items-center justify-center gap-4 p-4">
+          {data?.images.map((image) => (
+            <Image key={image} src={image} alt={data?.title} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
 
 export default Test;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch(BASE_URL + "/products");
-  const data: IGetProductsResponse = await response.json();
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const response = await fetch(BASE_URL + "/products");
+//   const data: IGetProductsResponse = await response.json();
 
-  const paths = data.products.map((product) => ({
-    params: { id: product.id.toString() },
-  }));
+//   const paths = data.products.map((product) => ({
+//     params: { id: product.id.toString() },
+//   }));
 
-  console.log("paths", paths);
+//   console.log("paths", paths);
 
-  return { paths, fallback: false };
-};
+//   return { paths, fallback: true };
+// };
 
-export const getStaticProps: GetStaticProps<{ data: IProduct }> = async ({
+// export const getStaticProps: GetStaticProps<{ data: IProduct }> = async ({
+//   params,
+// }) => {
+//   const id = params?.id;
+//   console.log("id inside getStaticProps : ", id);
+
+//   const response = await fetch(BASE_URL + `/products/${id}`);
+//   const data: IProduct = await response.json();
+
+//   console.log("data inside getStaticProps : ", data);
+
+//   return { props: { data } };
+// };
+
+export const getServerSideProps: GetServerSideProps<{ data: IProduct }> = async ({
   params,
 }) => {
   const id = params?.id;
